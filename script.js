01101fc8ce81c8c0b2e1a23508877ae3a24d94cf
Kamolkhon <kmominov@bk.ru>
2023-04-16 12:39:03 +0500

    first_commint

---
+++ script.js
@@ -0,0 +1,43 @@
+// const arr = [1, 2, 3];
+// console.log(arr[1]);
+
+// const mass = ["Kamolxon", "Muminov", "Ziyodjon o'g'li"];
+// console.log(mass[2]);
+
+
+// const obj = {
+//     'Anna': 500,
+//     'Alice': 800
+// }
+
+// const storeName = 'My cool Store';
+// const storeDescription = {
+//     open: true,
+//     budget: 10000,
+//     employees: ['Kamal', 'Mumin'],
+//     products: {
+//         water: 100,
+//         bread: 250
+//     }
+// }
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18 years old?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+// answers[0] = prompt("What is your name?");
+// answers[1] = prompt("What is your Second name?");
+// answers[2] = prompt("What is your age?");
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+const user = "Kamal";
+alert(`Hello, ${user}`);
